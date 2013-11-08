bf24dc978d9801a45700789ba352d46ac360f9b6
Loucher <matus.svitek@gmail.com>
2013-11-08 15:47:28 +0100

    uprava minSDK

--- Appka/src/main/java/com/pim3/appka/MainActivity.java
+++ Appka/src/main/java/com/pim3/appka/MainActivity.java
@@ -17,6 +17,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
+
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
