00685a712b28c9c612ff01df7e7e58a0c7b1b91a
Loucher <matus.svitek@gmail.com>
2013-11-08 22:52:30 +0100

    scalovanie

--- Appka/src/main/java/com/pim3/appka/MainActivity.java
+++ Appka/src/main/java/com/pim3/appka/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loader);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment(getApplicationContext()))
